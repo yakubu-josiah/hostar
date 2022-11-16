d7f67d8673e022aaf91802c051c0db76a0370952
yakubu-josiah <jossysmothest007@gmail.com>
2022-11-16 16:10:13 +0100

    adding the footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,94 @@
+<footer class="wpo-site-footer">
+    <!-- Footer Widgets -->
+    <div class="upper-footer">
+        <div class="footer-widget-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-3 col-md-3 col-sm-6">
+                        <div class=" widget text-widget">
+                            <div class="widget about-widget ">
+                                <div class="logo widget-title">
+                                    <img src="{{ asset('images/tr-logo.png') }}"
+                                        alt="">
+                                </div>
+                                <p>High Quality Hosting WordPress Theme in Envato Market. We offer Best Web
+                                    Hosting &amp; Domain.</p>
+                                <div class="social-icons">
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                        </div> <!-- end widget -->
+                    </div>
+                    <div class="col col-lg-3 col-md-3 col-sm-6">
+                        <div class="widget_text link-widget resource-widget widget widget_custom_html">
+                            <div class="widget-title">
+                                <h3>Resources</h3>
+                            </div>
+                            <div class="textwidget custom-html-widget">
+                                <ul>
+                                    <li><a href="#">WordPress Hosting</a></li>
+                                    <li><a href="#">Cloud Hosting</a></li>
+                                    <li><a href="#">Shared Hosting</a></li>
+                                    <li><a href="#">VPS Hosting</a></li>
+                                    <li><a href="#">Dedicated Hosting</a></li>
+                                </ul>
+                            </div>
+                            <div class="clear"></div>
+                        </div> <!-- end widget -->
+                    </div>
+                    <div class="col col-lg-3 col-md-3 col-sm-6">
+                        <div class="widget_text link-widget  widget widget_custom_html">
+                            <div class="widget-title">
+                                <h3>Useful Links</h3>
+                            </div>
+                            <div class="textwidget custom-html-widget">
+                                <ul>
+                                    <li><a href="{{ route('about') }}">About Us</a></li>
+                                    <li><a href="https://wpocean.com/wp/hostar/hosting/">Our Services</a></li>
+                                    <li><a href="https://wpocean.com/wp/hostar/pricing/">Our Pricing</a></li>
+                                    <li><a href="https://wpocean.com/wp/hostar/blog/">Latest News</a></li>
+                                    <li><a href="https://wpocean.com/wp/hostar/contact/">Contact Us</a></li>
+                                </ul>
+                            </div>
+                            <div class="clear"></div>
+                        </div> <!-- end widget -->
+                    </div>
+                    <div class="col col-lg-3 col-md-3 col-sm-6">
+                        <div class=" widget text-widget">
+                            <div class="widget-title">
+                                <h3>Contact</h3>
+                            </div>
+                            <div class="contact-widget service-link-widget">
+                                <ul>
+                                    <li>online store with lots of cool and exclusive wp-features</li>
+                                    <li><span>Address:</span>28 Street, New York City, USA</li>
+                                    <li><span>Phone:</span>+000123456789</li>
+                                    <li><span>Email:</span>hostar@gmail.com</li>
+                                </ul>
+                            </div>
+                            <div class="clear"></div>
+                        </div> <!-- end widget -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer Widgets -->
+    <div class="wpo-lower-footer  has-not-copyright text-center">
+        <div class="container">
+            <div class="row">
+                <div class="separator"></div>
+                <div class="col col-xs-12">
+                    <p>&copy; Copyright 2022 | <a href="https://wpocean.com/wp/hostar">Hostar – Web Hosting</a>
+                        | All right reserved.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
