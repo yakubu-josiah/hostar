6f3a434dbc63785f62baeda1b41bde9d04f52526
yakubu-josiah <jossysmothest007@gmail.com>
2022-12-16 12:42:38 +0100

    adding the back button in nametag component

--- resources/views/components/admin/nametag.blade.php
+++ resources/views/components/admin/nametag.blade.php
@@ -1,5 +1,7 @@
 <ul class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#"><i class="feather icon-home"></i></a>
+    <li class="breadcrumb-item"><a href="{{ route('adminDash') }}"><i class="feather icon-home"></i> home of </a>
     </li>
-    <li class="breadcrumb-item"><a href="javascript:">home of </a></li>
 </ul>
+<div>
+    <div class="justify-content-between"><a href="{{ url()->previous() }}" style="background-image:linear-gradient(150deg, #4c4097 0%, #a494f5 100%);color:#fff;border-radius:7px;" class="text-top p-2"><i class="feather icon-chevrons-left" style="font-size:20px; padding-top:5px;"></i> Go Back </a></div>
+</div>
